385b848981125cfd600ba60f128e58ca3312d03b
volosevych <volosevichh@gmail.com>
2024-12-30 21:50:08 -0700

    commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone", // Use dynamic rendering
     experimental: {
-      appDir: true, // Enable app directory if you're using it
+
     },
   };
   
   export default nextConfig;
+  
   
