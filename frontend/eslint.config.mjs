fc6b42bfa9a9f099df7d1ba4f0e68492b2b70ce5
volosevych <volosevichh@gmail.com>
2025-01-01 16:16:30 -0700

    commit

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,14 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    extends: ["next/core-web-vitals"],
+  },
+];
 
 export default eslintConfig;
